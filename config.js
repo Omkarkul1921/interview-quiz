b90ba364b82f653a8fedee340e2f078b7187362f
Omkarkul1921 <108816423+Omkarkul1921@users.noreply.github.com>
2025-11-04 13:52:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 const GITHUB_CONFIG = {
     owner: "Omkarkul1921",        // Change this to your GitHub username
     repo: "interview-quiz",               // Your repository name
-    token: "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",           // Generate from GitHub Settings > Developer Settings > Personal Access Tokens
+    token: "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",
     branch: "main"
 };
 
